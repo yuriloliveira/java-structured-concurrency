6d121655b6f3727c44aa76818b2c2e97337a20de
yuriloliveira <yuri12194@gmail.com>
2025-11-26 12:00:35 +0100

    adds missing logs in unstructured concurrency.

--- src/main/java/me/yurioliveira/examples/UnstructuredConcurrency.java
+++ src/main/java/me/yurioliveira/examples/UnstructuredConcurrency.java
@@ -14,18 +14,21 @@ void main() throws InterruptedException {
 
     Result<Profile.Details> detailsResult = Result.notReady();
     Thread detailsThread = builder.start(() -> {
+        log("Loading details...", ANSI_YELLOW);
         detailsResult.set(profile.loadDetails());
         log("Finished after %d ms", ANSI_YELLOW, counter.elapsed().toMillis());
     });
 
     Result<Integer> followersCountResult = Result.notReady();
     Thread followersCountThread = builder.start(() -> {
+        log("Loading followers count...", ANSI_PURPLE);
         followersCountResult.set(profile.loadFollowerCount());
         log("Finished after %d ms", ANSI_PURPLE, counter.elapsed().toMillis());
     });
 
     Result<List<Follower>> followersResult = Result.notReady();
     Thread followersThread = builder.start(() -> {
+        log("Loading followers...", ANSI_BLUE);
         followersResult.set(profile.loadFollowers());
         log("Finished after %d ms", ANSI_BLUE, counter.elapsed().toMillis());
     });
